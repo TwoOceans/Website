12f75307932b1764e4b41ea3df927bec3b30ec72
Bergbok <66174189+Bergbok@users.noreply.github.com>
2025-07-10 12:47:31 +0200

    fix: remove deprecated Sass API option from Vite config
    https://vite.dev/guide/migration.html#removed-sass-legacy-api-support

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
     appType: 'spa',
-    css: {
-        preprocessorOptions: {
-            sass: {
-                api: 'modern-compiler',
-            }
-        }
-    },
     plugins: [
         vue()
     ],
