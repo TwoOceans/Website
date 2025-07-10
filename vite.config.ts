35c709da2fc9e589b35417dd0144a08fbdfb0a2d
Bergbok <66174189+Bergbok@users.noreply.github.com>
2025-07-10 15:12:17 +0200

    feat: switch from GitHub Pages to Cloudflare Workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import { cloudflare } from '@cloudflare/vite-plugin';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
     appType: 'spa',
     plugins: [
+        cloudflare(),
         vue()
     ],
     server: {
